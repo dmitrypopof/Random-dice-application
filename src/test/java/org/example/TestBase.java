c3dfa26373f2a26b33ba9836db0d13bfbd11dd90
d.popov <dmitrypopof@mail.ru>
2025-03-03 21:20:31 +0300

    step 3

---
+++ src/test/java/org/example/TestBase.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.support.PageFactory;
+
+import java.net.MalformedURLException;
+
+public class TestBase {
+    ApplicationManager app;
+
+    public AndroidDriver driver;
+    private DriverFactory driverFactory = new DriverFactory();
+
+    @BeforeEach
+    public void setDriver() throws MalformedURLException {
+
+        driver = driverFactory.setUp();
+        // for annotations AndroidFindBy:
+        AppiumFieldDecorator appiumFieldDecorator = new AppiumFieldDecorator(driver);
+        PageFactory.initElements(appiumFieldDecorator,this);
+
+        app = new ApplicationManager(driver);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        //driver.terminateApp("ru.uxapps.random");
+        driver.quit();
+    }
+
+
+
+
+}
